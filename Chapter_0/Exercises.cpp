8c3d8f11383f924fd919690510a26ed9322729b5
Ricardo Marques <marques.ricardo17@gmail.com>
2019-03-18 18:04:24 +0100

    Chapter 0 folder and files.

---
+++ Chapter_0/Exercises.cpp
@@ -0,0 +1,11 @@
+// Program for the exercises at the end of Chapter 0
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "This (\") is a quote, \t and this (\\) is a backslash. " << std::endl;
+
+    return 0; 
+
+}
---
+++ Chapter_0/HelloWorld.cpp
@@ -0,0 +1,11 @@
+// First C++ program of this course.
+
+#include <iostream>
+
+int main() {
+
+std::cout << "Hello World!!" << std::endl;
+
+return 0;
+
+}
